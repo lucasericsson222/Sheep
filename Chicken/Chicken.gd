2ed289538727796c80a938eb5fe6e9ab33b1d650
skyfire222 <skyfire222@users.noreply.github.com>
2021-07-26 16:31:29 -0400

    Random Stuff?

--- Chicken/Chicken.gd
+++ Chicken/Chicken.gd
@@ -1,16 +1,53 @@
 extends KinematicBody2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
+var direction = Vector2(0,0)
+onready var screen_size = get_viewport_rect().size
+var rng = RandomNumberGenerator.new()
+var chicken_speed = 200
+var current_direction = Vector2(0,0)
 func _ready():
-	pass # Replace with function body.
+	rng.randomize()
+	
+	# initialize a random direction to start movement in that direction
+	var random_direction2 = rng.randf_range(0, 2 * PI)
+	current_direction.x = cos(random_direction2)
+	current_direction.y = sin(random_direction2)
+	$ChangeDirTimer.wait_time += rng.randf_range(-0.5, 0.5)
+	# intitialize random direction to turn to
+	_on_ChangeDirTimer_timeout()
+	chickenColorize()
 
+func chickenColorize():
+	
+	var random_brighten_percent = rng.randf_range(0.3,0.5)
+# apply color randomness
+	$AnimatedSprite.modulate = $AnimatedSprite.modulate.blend(Color(1.0,1.0,1.0,random_brighten_percent))
+	
+func _process(delta):
+	
+	# slowly change directions to the new direction
+	current_direction = lerp(current_direction, direction, 0.01)
+	# apply the direction
+	position += delta * current_direction * chicken_speed 
+	screenWrap()
+	# point the chicken in the right direction
+	$AnimatedSprite.flip_h = (current_direction.dot(Vector2(1,0)) > 0)
+	z_index = position.y
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_ChangeDirTimer_timeout():
+	# generate a new random direction to head
+	var random_direction = rng.randf_range(0, 2 * PI)
+	direction.x = cos(random_direction)
+	direction.y = sin(random_direction)
+	# start a time till change the direction again
+	$ChangeDirTimer.start()
+
+func screenWrap():
+	if (position.x > screen_size.x):
+		position.x = 0
+	if position.x < 0:
+		position.x = screen_size.x
+	if position.y > screen_size.y:
+		position.y = 0
+	if position.y < 0:
+		position.y = screen_size.y
--- Sheep/Sheep.gd
+++ Sheep/Sheep.gd
@@ -11,16 +11,23 @@ onready var carea = $CohesionArea
 onready var sarea = $SeparationArea
 onready var aarea = $AdhesionArea
 onready var tarea = $TargetArea
-
+export var MAX_SPEED = 200
+# initialize
 func _ready():
 	rng.randomize()
+	
+	# randomize starting frames
 	anispr.frame = rng.randi_range(0,5)
+	
+	# randomize starting direction
 	var start_angle = rng.randf_range(0.0, 2 * PI)
 	direction.x = 1000 * cos(start_angle)
 	direction.y = 1000 * sin(start_angle)
+	
+	# randomize colors
 	sheepColorize()
 
-
+# color the sheep different colors
 func sheepColorize():
 	
 	var random_brighten_percent = rng.randf_range(0.0,0.5)
@@ -31,7 +38,7 @@ func sheepColorize():
 	anispr.modulate = anispr.modulate.blend(Color(1.0,1.0,1.0,random_brighten_percent))
 	anispr.modulate = anispr.modulate.blend(Color(0.0,0.0,0.0,random_darken_percent))
 
-
+# all sheep move towards center of mass of nearby sheep
 func cohesion():
 	var neighbor_sheep = carea.get_overlapping_bodies()
 	var total_position = Vector2(0,0)
@@ -47,15 +54,16 @@ func cohesion():
 		return (average_position-position)/100
 	else:
 		return Vector2(0,0)
+# all sheep move out of the way of other very nearby sheep
 func separation():
 	var move_away = Vector2(0,0)
 	var neighbor_sheep = sarea.get_overlapping_bodies()
 	for sheep in neighbor_sheep:
-		if sheep == self || !sheep.is_in_group("Sheep"):
+		if sheep == self || (!sheep.is_in_group("Sheep") && !sheep.is_in_group("Chicken")):
 			continue
 		move_away -= (sheep.position - position)
 	return move_away
-
+# all sheep point in the general direction of nearby sheep
 func adhesion():
 	var neighbor_sheep = aarea.get_overlapping_bodies()
 	var total_velocity = Vector2(0,0)
@@ -69,6 +77,7 @@ func adhesion():
 	if number_of_sheep != 0:
 		average_velocity = total_velocity / number_of_sheep
 	return average_velocity / 8
+# all sheep move toward the target
 func target():
 	var neighbor_sheep = tarea.get_overlapping_bodies()
 	var total_position = Vector2(0,0)
@@ -85,30 +94,36 @@ func target():
 	else:
 		return Vector2(0,0)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+# collect all the cohesion, separation, adhesion, and target rules together
+func run():
+	# FLOCKING RULES
+	direction += cohesion()
+	direction += separation()
+	direction += adhesion()
+	direction += target()
+	# direction /= 1.01
+	# makes sure that the sheep don't go lightspeed
+	direction = direction.clamped(MAX_SPEED)
 
+# apply position changes and manage states
 func _process(delta):
+	# if the state == run
 	if anispr.animation == "run":
-# FLOCKING RULES
-		direction += cohesion()
-		direction += separation()
-		direction += adhesion()
-		direction += target()
-# makes sure that the sheep don't go lightspeed
-		direction = direction.clamped(200) 
-#	direction /= 1.01
-# delta ensures that the sheep move at the same speed on different devices
-	position += direction * delta 
-# if the sheep is going in a different direction, change which way the sprite is facing
+		run()
+		# condition to go to rolling
+		if (rng.randf_range(1.0, 500.0) < 2.0):
+			anispr.play("roll")
+			anispr.speed_scale = 2
+	
+	# if the sheep is going in a different direction, change which way the sprite is facing
 	anispr.flip_h = !(direction.dot(Vector2(1,0)) > 0)
-	screenWrap()
-# make sure that sheep appear in the right order on the screen
+	# make sure that sheep appear in the right order on the screen
 	z_index = position.y
-	if (rng.randf_range(1.0, 500.0) < 2.0):
-		anispr.play("roll")
-		anispr.speed_scale = 2
-	
-	
+	# delta ensures that the sheep move at the same speed on different devices
+	position += direction * delta 
+	screenWrap()
+
+# if the sheep is off the screen move it to the other side of the screen
 func screenWrap():
 	if (position.x > screen_size.x):
 		position.x = 0
@@ -119,8 +134,7 @@ func screenWrap():
 	if position.y < 0:
 		position.y = screen_size.y
 
-
-
+# condition to go back to running from rolling
 func _on_AnimatedSprite_animation_finished():
 	if anispr.animation == "roll":
 		anispr.play("run")
